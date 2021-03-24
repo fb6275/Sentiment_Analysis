f4c727fac375197723cabea709fd07ed1a2800b5
fb6275 <75378749+fb6275@users.noreply.github.com>
2021-03-24 16:31:24 +0100

    update app_scrap.py

--- app_scrap.py
+++ app_scrap.py
@@ -1,17 +1,30 @@
 import flask
 from flask import request, jsonify
 import original
+import json
 
 
 app = flask.Flask(__name__)
           
-@app.route('/')
-def scrapapi():
-     df1 = original.scraping()
+@app.route('/<category>')
+def scrapapi(category):
+     df1 = original.scraping(category) 
      df1 = original.nlp(df1)
-     return str(df1)
+     # Wordcloud
+     # cloud = original.wordcloud(df1)
+     count_value = original.the_count(df1['score'])
+     title_shop = original.the_count(df1['nom_shop'])
+     title_shop = df1['nom_shop'].values.tolist() 
+     
+     result = {}
+     result['score'] = str(count_value)
 
+     
+     return result
+     # print(flask.jsonify(id=count_value, title=title_shop))
+     # return flask.jsonify(id=str(count_value), title=str(title_shop))
 
+# 
                
 if __name__ == "__main__":
      # Launch the Flask dev server
